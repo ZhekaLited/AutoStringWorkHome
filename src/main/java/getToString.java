4bb2f68b49bfb069756312009eed89e9e2dfba91
ZhekaLited <94718651+ZhekaLited@users.noreply.github.com>
2022-04-22 14:43:16 +0300

    Version 0.1

---
+++ src/main/java/getToString.java
@@ -0,0 +1,38 @@
+public class getToString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int num) {
+        String str1 = "This is exercise 1";
+        String str2 = "This is Exercise 1";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+
+        // Compare the two strings.
+        int result = str1.compareToIgnoreCase(str2);
+
+        // Display the results of the comparison.
+        if (result < 0) {
+            System.out.println("\"" + str1 + "\"" +
+                    " is less than " +
+                    "\"" + str2 + "\"");
+            return result;
+        } else if (result == 0) {
+            System.out.println("\"" + str1 + "\"" +
+                    " is equal to " +
+                    "\"" + str2 + "\"");
+            return result;
+        } else // if (result > 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is greater than " +
+                    "\"" + str2 + "\"");
+        }
+        return result;
+    }
+}
+
+
---
+++ src/test/java/getToStringTest.java
@@ -0,0 +1,24 @@
+import org.assertj.core.internal.Numbers;
+import org.assertj.core.internal.bytebuddy.implementation.bind.annotation.This;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class getToStringTest {
+
+
+    @Test
+  public void reduce() {
+       assertEquals(-1,getToString.add(18));
+    }
+
+    @Test
+    public void truee () {
+        assertEquals(0,getToString.add(18));
+    }
+
+    @Test
+    public void increase() {
+        assertEquals(2,getToString.add(19));
+    }
+}
